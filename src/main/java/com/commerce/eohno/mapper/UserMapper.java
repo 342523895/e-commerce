af4ec12ccfff6e42d3de6493d27d63e3160cd861
万达 <da.wan@sinosdx.com>
2019-06-02 16:32:23 +0800

    first alter

---
+++ src/main/java/com/commerce/eohno/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.commerce.eohno.mapper;
+
+import com.commerce.eohno.pojo.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/commerce/eohno/pojo/User.java
@@ -0,0 +1,23 @@
+package com.commerce.eohno.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    private String email;
+
+    private Date created;
+
+    private Date updated;
+
+}
